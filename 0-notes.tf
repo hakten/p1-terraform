fc3e29abe52e2f4f8aae1acc7421852b4f6edc35
Huseyin Akten <husakten@gmail.com>
2019-12-26 02:36:47 -0600

    vars

--- 0-notes.tf
+++ 0-notes.tf
@@ -1,7 +1,5 @@
-variable "mylist" {
-  type  = "${list(value1, value2)}"
+
+variable "mysecondlist" {
+  type = "list"
+  default = ["value01", "value02"]
 }
-//variable "mysecondlist" {
-//  type = "list"
-//  default = [value01, value02]
-//}
