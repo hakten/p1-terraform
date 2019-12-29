7f35feccfeb23a1a5d50d36aeb1679c0c0765d2e
Huseyin Akten <husakten@gmail.com>
2019-12-29 13:14:25 -0600

    route added

--- 5-vpc-igw-route.tf
+++ 5-vpc-igw-route.tf
@@ -7,3 +7,13 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route" "pub_route" {
+  route_table_id         = aws_vpc.vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
--- new.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-}
-
-resource "aws_route" "pub_route" {
-  route_table_id         = aws_vpc.vpc.main_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
