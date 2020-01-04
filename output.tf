97242cb593bf39b150ed0a59ca1aef4c1d7a27db
Huseyin Akten <husakten@gmail.com>
2020-01-03 19:28:59 -0600

    output added to module

---
+++ output.tf
@@ -0,0 +1,12 @@
+output "first-vpc-id" {
+  value = module.module-first-vpc.aws_vpc.vpc.id
+}
+output "first-vpc-cidr-block" {
+  value = module.module-first-vpc.aws_vpc.vpc.cidr_block
+}
+output "second-vpc-id" {
+  value = module.module-second-vpc.aws_vpc.vpc.id
+}
+output "second-vpc-cidr-block" {
+  value = module.module-second-vpc.aws_vpc.vpc.cidr_block
+}
