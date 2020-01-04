16b030e0c73c5320691f36002ea2f895b9fcecc7
Huseyin Akten <husakten@gmail.com>
2020-01-03 20:09:05 -0600

    asdf

--- third-peering/peering.tf
+++ third-peering/peering.tf
@@ -2,6 +2,6 @@ resource "aws_vpc_peering_connection" "peering" {
   vpc_id        = var.output.first-vpc-id
   region        = "us-east-2"
   peer_vpc_id   = var.output.second-vpc-id
-  #peer_region   = "us-east-1"
+  peer_region   = "us-east-1"
   auto_accept   = true
 }
