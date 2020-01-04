bf5fa3b2a3ea925de23e1b3bfa69148e4c9946c9
Huseyin Akten <husakten@gmail.com>
2020-01-03 20:06:26 -0600

    region added

--- third-peering/peering.tf
+++ third-peering/peering.tf
@@ -2,4 +2,5 @@ resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id   = var.output.second-vpc-id
   vpc_id        = var.output.first-vpc-id
   auto_accept   = true
+  peer_region   = "us-east-1"
 }
