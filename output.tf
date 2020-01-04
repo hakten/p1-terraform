89eb673e18b7222e6780d3259ac360fa5be04228
Huseyin Akten <husakten@gmail.com>
2020-01-03 19:33:54 -0600

    output module

--- output.tf
+++ output.tf
@@ -1,12 +1,12 @@
 output "first-vpc-id" {
-  value = module.module-first-vpc.aws_vpc.vpc.id
+  value = "lslsl"
 }
-output "first-vpc-cidr-block" {
-  value = module.module-first-vpc.aws_vpc.vpc.cidr_block
-}
-output "second-vpc-id" {
-  value = module.module-second-vpc.aws_vpc.vpc.id
-}
-output "second-vpc-cidr-block" {
-  value = aws_vpc.vpc.cidr_block
-}
+# output "first-vpc-cidr-block" {
+#   value = module.module-first-vpc.aws_vpc.vpc.cidr_block
+# }
+# output "second-vpc-id" {
+#   value = module.module-second-vpc.aws_vpc.vpc.id
+# }
+# output "second-vpc-cidr-block" {
+#   value = aws_vpc.vpc.cidr_block
+# }
