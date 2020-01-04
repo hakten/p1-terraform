d95c0319fd2f2e3232298e2369e945f43320d73f
Huseyin Akten <husakten@gmail.com>
2020-01-03 19:30:56 -0600

    sdfas

--- output.tf
+++ output.tf
@@ -8,5 +8,5 @@ output "second-vpc-id" {
   value = module.module-second-vpc.aws_vpc.vpc.id
 }
 output "second-vpc-cidr-block" {
-  value = module.module-second-vpc.aws_vpc.vpc.cidr_block
+  value = module-second-vpc.aws_vpc.vpc.cidr_block
 }
