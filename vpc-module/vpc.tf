42850dc585c078192f374013ad30eeda3ddc6fa4
Huseyin Akten <husakten@gmail.com>
2020-01-07 23:05:05 -0600

    name changed

--- vpc-module/vpc.tf
+++ vpc-module/vpc.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = "true"
 
   tags = {
-    Name = var.environment-var.vpc-name
+    Name = ${var.environment}-${var.vpc-name}
     Environment = var.environment
   }
 }
