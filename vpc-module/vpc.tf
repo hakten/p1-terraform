312e867e511efcdc53f8a4c114db6ead5ebd5d0c
Huseyin Akten <husakten@gmail.com>
2020-01-07 23:10:50 -0600

    vpc added

--- vpc-module/vpc.tf
+++ vpc-module/vpc.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = "true"
 
   tags = {
-    Name = "${var.environment}"
+    Name = "${var.environment}"-"${var.vpc-name}"
     Environment = var.environment
   }
 }
